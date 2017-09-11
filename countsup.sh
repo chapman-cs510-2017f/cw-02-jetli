a49038b32c17cd53e75d995254a2b2441a4dd56d
Jet Li <jiali@chapman.edu>
2017-09-11 16:38:32 +0000

    add countsup.sh

---
+++ countsup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read myinput
+echo $myinput
+
+
+if [[ $myinput =~ ^[0-9]+$ ]]; then
+    echo "this is positive interger"
+	for i in $(seq $myinput); do echo '' $i''; done
+	exit 0 # This exits as a success
+else
+    echo "Sorry, positive integers only"
+	exit 1
+fi
+
+
+
+
+#=~ is the "regular expression match" operator
+#^ matches with beginning of string
+#[0-9] matches a digit
+#+ one or more of the previous (i.e. digit)
+#$ end of string
